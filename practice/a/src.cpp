b39c98900f2992b23ce9e5a10ca956ebf6ff680b
ysuzuki19 <msethuuh7@i.softbank.jp>
2019-03-31 04:32:51 +0900

    add practice a

---
+++ practice/a/src.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv){
+ int a, b, c;
+ string s;
+ cin >> a >> b >> c >> s;
+ cout << a+b+c << " " << s << endl;
+ return 0;
+}

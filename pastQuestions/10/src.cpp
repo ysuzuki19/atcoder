e63611fef08e9587b95859128955fed8edfc5c98
ysuzuki19 <msethuuh7@i.softbank.jp>
2019-04-13 01:47:51 +0900

    blash up from mac

--- pastQuestions/10/src.cpp
+++ pastQuestions/10/src.cpp
@@ -2,23 +2,17 @@
 using namespace std;
 
 int main(){
-  // start process
-  int n;
-  int i;
+  int n, i;
   cin >> n;
-  vector<int> t(n);
-  vector<int> x(n);
-  vector<int> y(n);
-  t[0] = 0;
-  x[0] = 0;
-  y[0] = 0;
-  for(i=1; i<n+1; i++){
-    cin >> t[i] >> x[i] >> y[i];
-  }
   int pastEO = 0;
   int currEO = 0;
   int timeEO = 0;
   int necessaryMove = 0;
+  vector<int> t(n), x(n), y(n);
+  t[0] = x[0] = y[0] = 0;
+  for(i=1; i<n+1; i++){
+    cin >> t[i] >> x[i] >> y[i];
+  }
   for(i=1; i<n+1; i++){
     necessaryMove = abs(x[i]-x[i-1]) + abs(y[i]-y[i-1]);
     if(necessaryMove > t[i] - t[i-1]){
@@ -28,8 +22,7 @@ int main(){
     timeEO = (t[i]-t[i-1])%2;
     currEO = (x[i]+y[i])%2;
     if(pastEO == currEO){
-      if(timeEO == 0){
-      }else{
+      if(timeEO != 0){
         cout << "No" << endl;
         return 0;
       }
@@ -37,12 +30,10 @@ int main(){
       if(timeEO == 0){
         cout << "No" << endl;
         return 0;
-      }else{
       }
     }
     pastEO = currEO;
   }
   cout << "Yes" << endl;
-  // end process
   return 0;
 }
